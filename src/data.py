75d1ab43f96b565e3722e9147eca3203615ef37c
YassineGharabi <gharabiyassine18@gmail.com>
2025-09-30 16:25:47 +0100

    Add update delete menu

--- src/data.py
+++ src/data.py
@@ -4,8 +4,5 @@ produits = np.array([
     [ "Smart phone" , 30 , 2000 ],
     [ "Tv" , 15 , 4000 ],
     [ "voiture" , 10 , 25000 ],
-    [ "clavier" , 30 , 500 ],
-    ['Apple', '10', '1.50'],
-    ['Banana', '15', '0.75'], 
-    ['Orange', '8', '2.00']
+    [ "clavier" , 30 , 500 ]
 ])
--- src/stock.py
+++ src/stock.py
@@ -1,4 +1,4 @@
-
+from data import produits
 import numpy as np
 
 def ajouter_produit():
@@ -12,9 +12,50 @@ def ajouter_produit():
 
 def afficher():
     global produits
-    for item in produits :
-        print(item)
-    
+    for produit in produits :
+        print(f"{produit[0]} | {produit[1]} Qte | {produit[2]} DH")
+
+
+def mettre_a_jour_quantite():
+    global produits
+
+    nom_produit = input("Enter the name of product you want to update : ")
+    exists = False
+    index = 0
+    for i in range(len(produits)):
+        if produits[i][0].lower() == nom_produit.lower():
+            index = i
+            exists = True
+
+    if(exists):
+        new_qte = int(input("Enter the new qte : "))
+        produits[index][1] = new_qte
+        print("Updated")
+    else:
+        print("Product not exist")
+
+
+def supprimer_produit():
+    global produits
+
+    nom_produit = input("Enter the name of produit you want to delete : ")
+
+    exists = False
+    index = 0
+
+    for i in range(len(produits)):
+        if produits[i][0].lower() == nom_produit.lower():
+            exists = True
+            index = i
+        else:
+            exists = False
+
+    if(exists):
+        produits = np.delete(produits,index,axis=0)
+        print("deleted")
+    else:
+        print("Not exists")
+
 
 
 
